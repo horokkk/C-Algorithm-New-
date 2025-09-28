b3a79b506144dc82d155a9b7e31705c9b311ca75
Kim Jiyoon <smurfystrawberry@gmail.com>
2025-09-29 00:13:55 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/9498. 시험 성적/시험 성적.cc
+++ C++17/백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -5,9 +5,9 @@ int main(){
     int x;
     cin >>x;
     
-    if(x>=90){cout <<"A" <<endl;}
-    else if(x>=80){cout <<"B" <<endl;}
-    else if(x>=70){cout <<"C" <<endl;}
-    else if(x>=60){cout <<"D" <<endl;}
-    else{cout <<"F" <<endl;}
+    if(x>=90)cout <<"A";
+    else if(x>=80)cout <<"B";
+    else if(x>=70)cout <<"C";
+    else if(x>=60)cout <<"D";
+    else cout <<"F" <<endl;
 }
