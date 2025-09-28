1f94d796bf18d7f48cc281b600793c51152537a8
Kim Jiyoon <smurfystrawberry@gmail.com>
2025-09-28 23:49:06 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cin >>x;
+    
+    if(x%4==0 && x%100!=0){cout <<"1" <<endl;}
+    else if(x%400==0){cout <<"1" <<endl;}
+    else {cout <<"0" <<endl;}
+}
