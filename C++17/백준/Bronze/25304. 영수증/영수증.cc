5066a0b0a03437298f6250b65d2eb07fd9ea5c8e
Kim Jiyoon <smurfystrawberry@gmail.com>
2025-10-01 23:23:06 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int X, N, a, b;
+    int sum=0;
+    cin >>X;
+    cin >>N;
+    
+    for(int i=0;i<N; i++){
+        cin >>a >>b;
+        sum+=a*b;
+    }
+    
+    (sum==X)? cout <<"Yes" : cout <<"No";
+}
