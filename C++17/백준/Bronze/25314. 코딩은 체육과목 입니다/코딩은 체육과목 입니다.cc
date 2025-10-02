39305caf556f3b58a6bbfa17f24dbee4dc8586ea
Kim Jiyoon <smurfystrawberry@gmail.com>
2025-10-02 23:36:45 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N, n;
+    cin >>N;
+    n=N/4;
+    
+    for(int i=0;i<n;i++){
+        cout <<"long ";
+    }
+    cout <<"int";
+    
+}
