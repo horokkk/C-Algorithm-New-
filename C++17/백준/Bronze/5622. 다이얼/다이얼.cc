9a607a55c670ecbc5bf3efb717275b872102837e
Kim Jiyoon <smurfystrawberry@gmail.com>
2025-11-09 23:10:26 +0900

    [Bronze II] Title: 다이얼, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- C++17/백준/Bronze/5622. 다이얼/다이얼.cc
+++ C++17/백준/Bronze/5622. 다이얼/다이얼.cc
@@ -4,19 +4,20 @@ using namespace std;
 
 int main(){
     string s;
-    int num = 0;
+    int ans = 0;
     cin >>s;
     
-    for(size_t i=0;i<s.size();i++){
-        if(s[i]=='A'||s[i]=='B'||s[i]=='C') num+=3;
-        else if(s[i]=='D'||s[i]=='E'||s[i]=='F') num+=4;
-        else if(s[i]=='G'||s[i]=='H'||s[i]=='I') num+=5;
-        else if(s[i]=='J'||s[i]=='K'||s[i]=='L') num+=6;
-        else if(s[i]=='M'||s[i]=='N'||s[i]=='O') num+=7;
-        else if(s[i]=='P'||s[i]=='Q'||s[i]=='R'||s[i]=='S') num+=8;
-        else if(s[i]=='T'||s[i]=='U'||s[i]=='V') num+=9;
-        else if(s[i]=='W'||s[i]=='X'||s[i]=='Y'||s[i]=='Z') num+=10;
+    for(char ch:s){
+        char c=(unsigned char)ch;
+        if(c>='A'&&c<='C') ans+=3;
+        else if(c<='F') ans+=4;
+        else if(c<='I') ans+=5;
+        else if(c<='L') ans+=6;
+        else if(c<='O') ans+=7;
+        else if(c<='S') ans+=8;
+        else if(c<='V') ans+=9;
+        else if(c<='Z') ans+=10;
     }
     
-    cout <<num;
+    cout <<ans;
 }
